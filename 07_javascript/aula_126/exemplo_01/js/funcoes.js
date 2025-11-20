801ea987bbd496b26d730df83c3f5c6c5963c0a8
Carlos <carloscastro96@live.com.pt>
2025-11-20 23:22:56 +0000

    aula_126_notready

---
+++ 07_javascript/aula_126/exemplo_01/js/funcoes.js
@@ -0,0 +1,3 @@
+
+
+function mudar(){div.classList.toggle("active")};
---
+++ 07_javascript/aula_126/exemplo_02/js/funcoes.js
@@ -0,0 +1,12 @@
+
+function add(){
+  saida.innerHTML = "<h3>Olá</h3>";
+}
+
+function remove(){
+  saida.innerHTML = "<h3>Adeus</h3>";
+}
+
+function mudar(){
+  saida.classList.toggle("active");
+}
