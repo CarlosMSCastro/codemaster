0b003b05cb6486206bf63a09f8c6782742fe2128
Carlos <carloscastro96@live.com.pt>
2025-12-17 23:17:40 +0000

    aula_143

---
+++ 07_javascript/aula_143/exercicio_01/js/funcoes.js
@@ -0,0 +1,9 @@
+function ver_mais() {
+  document.getElementById('modal').classList.remove('d-none');
+  document.getElementById('vermais').classList.add('d-none');
+}
+
+function ver_menos() {
+  document.getElementById('modal').classList.add('d-none');
+  document.getElementById('vermais').classList.remove('d-none');
+}
