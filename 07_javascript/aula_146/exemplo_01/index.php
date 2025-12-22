1c663dbe1c7199e03fbb715c7e8e0cc9edaad751
Carlos <carloscastro96@live.com.pt>
2025-12-22 22:34:44 +0000

    aula_146

---
+++ 07_javascript/aula_146/exemplo_01/index.php
@@ -0,0 +1,66 @@
+
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Exercício 144.3</title>
+  <link rel="stylesheet" href="css/style.css">
+  <script src="js/funcoes.js"></script>
+</head>
+<body>
+  <div class="titulos">
+    <h1>Supermercado</h1>
+    <h1>2026</h1>
+  </div>
+  <input type="text" id="nome" placeholder="Nome do Produto" oninput="procurar()">
+  <div class="cards">
+    <div class="card">
+      <img src="img/pao.png" alt="pao">
+      <h3>Pão Bijou</h3>
+      <p class="preco">€0.13</p>
+    </div>
+    <div class="card">
+      <img src="img/ksurp.avif" alt="ksurp">
+      <h3>Kinder Surpresa</h3>
+      <p class="preco">€2.89</p>
+    </div>
+    <div class="card">
+      <img src="img/kbueno.avif" alt="kbueno">
+      <h3>Kinder Bueno</h3>
+      <p class="preco">€1.29</p>
+    </div>
+    <div class="card">
+      <img src="img/limiano.webp" alt="limiano">
+      <h3>Queijo Limiano</h3>
+      <p class="preco">€1.99</p>
+    </div>
+    <div class="card">
+      <img src="img/mnm.avif" alt="mnm">
+      <h3>M & M</h3>
+      <p class="preco">€1.79</p>
+    </div>
+    <div class="card">
+      <img src="img/pchou.webp" alt="pchou">
+      <h3>Pão com chouriço</h3>
+      <p class="preco">€1.49</p>
+    </div>
+    <div class="card">
+      <img src="img/picanha.png" alt="picanha">
+      <h3>Picanha</h3>
+      <p class="preco">€17.49</p>
+    </div>
+    <div class="card">
+      <img src="img/bolicau.png" alt="bolicau">
+      <h3>Bollycao</h3>
+      <p class="preco">€3.99</p>
+    </div>
+        <div class="card">
+      <img src="img/bolicaul.png" alt="bolicaul">
+      <h3>Bollycao Leite</h3>
+      <p class="preco">€3.99</p>
+    </div>
+  </div>
+
+</body>
+</html>
